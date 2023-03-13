667c8beb62b36c08719986117cf185fcc5f0fee9
AVShanaev <AVShanaev@gmail.com>
2023-03-13 23:30:25 +0300

    HW seminar6

---
+++ seminar6/project1/Program.cs
@@ -0,0 +1,34 @@
+﻿/* 
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+ */
+
+int getCountPositivNums(int num)
+{
+    int result = 0;
+    string temp = "";
+    Console.WriteLine("Для досрочного прекращения ввода введите: c" );
+
+    for (int i = 0; i < num; i++)
+    {
+        temp = Console.ReadLine();
+
+        if (temp == "c")
+        {
+            break;
+        } 
+
+        if (int.Parse(temp)>0)
+        {
+            result++;
+        }
+    } 
+
+    return result;
+}
+Console.Write("Введите колличество чисел для ввода: ");
+int num = int.Parse(Console.ReadLine());
+
+int countNegativNums = getCountPositivNums(num);
+Console.WriteLine(countNegativNums);
---
+++ seminar6/project2/Program.cs
@@ -0,0 +1,39 @@
+﻿/* 
+Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+- в контрольном примере ошибка:
+подставим данные  в уравнение
+-> -0,5 = 5 * - 0,5 + 2;
+-> -0,5 = -2,5+2
+ */
+
+void printCrosPoint(int a1, int b1, int a2, int b2)
+{
+    double x = 0;
+    double y = 0;
+
+    x = Convert.ToDouble(b1- b2)/Convert.ToDouble(a2-a1);
+    y = a2 * x + b2;
+
+    Console.WriteLine("Координаты точки перечений функций");
+    Console.WriteLine($"y = {a1}x + {b1}  и y = {a2}x + {b2}");
+    Console.WriteLine($"x = {x}; y = {y}");   
+}
+Console.WriteLine("Укажите параметны для 2х уравнений типа : F(x) = ax + b ");
+
+Console.Write("Введите первую константу для первой функции: ");
+int a1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите вторую константу для первой функции: ");
+int b1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите первую константу для второй функции: ");
+int a2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите вторую константу для второй функции: ");
+int b2 = int.Parse(Console.ReadLine());
+
+printCrosPoint(a1, b1, a2, b2);
